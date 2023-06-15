0de6db62f3250fcbbd8b0d71b0315c2232e99a89
ErandyRQ <ErandyRQ@gmail.com>
2023-06-15 11:28:29 -0600

    Modificación Controller Coctel

--- Examen-Coctel/Controller/CoctelesController.swift
+++ Examen-Coctel/Controller/CoctelesController.swift
@@ -12,16 +12,14 @@ class CoctelesController: UIViewController {
     @IBOutlet weak var itemCocteles: UICollectionView!
     
     
-    var drinks : [Drink] = []
-           var nombreFiltro : [Drink] = []
+           var drinks : [Drink] = []
+           var drinksAMostrar : [Drink] = []
            var Id : String = ""
            var Nombre : String = ""
            var Ingrediente1 : String = ""
            var Ingrediente2 : String = ""
            var Ingrediente3 : String = ""
            var instrucciones : String = ""
-           var drinksAMostrar : [Drink] = []
-           var isSearching = false
            var imgUrl = ""
    
     
@@ -63,34 +61,21 @@ extension CoctelesController: UICollectionViewDelegate, UICollectionViewDataSour
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-//        return drinks.count
-        if isSearching{
-            return drinksAMostrar.count
-        }
-        else{
             return drinks.count
-        }
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "coctelCell", for: indexPath) as! CoctelesCell
         
-        if isSearching{
-            cell.NombreCoctel.text = drinksAMostrar[indexPath.row].strDrink
-            
-            cell.imagenCoctel.image = UIImage(named: "\(drinksAMostrar[indexPath.row].strDrinkThumb)")
-        }
-        else{
+       
             cell.NombreCoctel.text = drinks[indexPath.row].strDrink
-//            cell.ImageView.image = UIImage(named: "\(drinks[indexPath.row].strDrinkThumb)")
-//            var imgUrl = URL(string: "www.thecocktaildb.com/images/ingredients/\(drinks[indexPath.row].strDrink)")!
             self.imgUrl = drinks[indexPath.row].strDrinkThumb
             let url = URL(string: "\(imgUrl)/preview")!
             cell.imagenCoctel.load(url: url)
-        }
         
-//        cell.lblNombre.text = drinks[indexPath.row].strDrink
-//        cell.ImageView.image = UIImage(named: "\(drinks[indexPath.row].strDrinkThumb)")
+        
+
         
         
         
@@ -104,31 +89,13 @@ extension CoctelesController: UICollectionViewDelegate, UICollectionViewDataSour
         print(Nombre)
         self.Ingrediente1 = drinks[indexPath.row].strIngredient1
         self.Ingrediente2 = drinks[indexPath.row].strIngredient2
-        self.Ingrediente3 = drinks[indexPath.row].strIngredient3 ?? "No hay mas ingrediente"
+        self.Ingrediente3 = drinks[indexPath.row].strIngredient3 ?? "No tiene ingrediente"
         self.instrucciones = drinks[indexPath.row].strInstructions
         self.imgUrl = drinks[indexPath.row].strDrinkThumb
            self.performSegue(withIdentifier: "SegueDescripcion", sender: self)
     }
     
-   /* override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-             
-              if segue.identifier == "SegueDescripcion"{
-                  let formControl = segue.destination as! CoctelDescripcionController
-                  formControl.idCoctel = self.Id
-                  formControl.Nombre = self.Nombre
-                  formControl.ingrediente1 = self.Ingrediente1
-                  formControl.ingrediente2 = self.Ingrediente2
-                  formControl.ingrediente3 = self.Ingrediente3
-                  formControl.instrucciones = self.instrucciones
-                  formControl.imgUrl = self.imgUrl
-                  
-                  
-                  
-//              }else{
-//                  let formcontrol1 = segue.destination as! ProductoGetAllCollectionController
-//                  formcontrol1.datotxt = guardardato
-              }
-          }*/
+  
 }
 extension UIImageView {
     func load(url: URL) {
